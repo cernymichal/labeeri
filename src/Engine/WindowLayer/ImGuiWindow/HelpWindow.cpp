f60f0c6322cc5c212d7a2791eec8a45b189b010b
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2023-05-22 05:16:38 +0200

    update help

--- src/Engine/WindowLayer/ImGuiWindow/HelpWindow.cpp
+++ src/Engine/WindowLayer/ImGuiWindow/HelpWindow.cpp
@@ -10,11 +10,14 @@ bool HelpWindow::draw() {
     bool shouldContinue = true;
     ImGui::Begin(m_name.c_str(), &shouldContinue);
 
-    ImGui::Text("Click into the scene or use ESC to return");
+    ImGui::Text("Click into the scene or use ESC to return back to UI");
     ImGui::Text("Use the WASD keys to move, sprint with left shift");
+    ImGui::Text("Left click to interact with objects");
+    ImGui::Text("F1 switch to flycam / player");
+    ImGui::Text("Page up / down go through static cameras");
     ImGui::Text("F3 to show/hide interface");
     ImGui::Text("F5 switches to fullscreen");
-    ImGui::Text("Ctrl + left click objects to edit them");
+    ImGui::Text("Ctrl + left click objects to edit them in UI mode");
 
     ImGui::End();
 
